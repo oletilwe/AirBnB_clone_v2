9510e31113df322233721abf2930ea7704dcbe5b
oletilwe <oletilwelephalala8@gmail.com>
2024-03-06 11:54:17 -0800

    ...

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,23 +8,29 @@ env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_deploy(archive_path):
-    """ distributes the archive to the server """
-    if not os.path.exists(archive_path):
+    if os.path.isfile(archive_path) is False:
+        return False
+    
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed is True:
         return False
 
-    try:
-         put(archive_path, "/tmp/")
-         out = archive_path[9:-4]
-         new_path = archive_path[9:]
-         releases = "/data/web_static/releases"
-         run(f"mkdir -p {releases}/{out}")
-         run(f"tar -xzf /tmp/{new_path} -C {releases}/{out}/")
-         run(f"rm /tmp/{new_path}")
-         run(f"mv {releases}/{out}/web_static/* {releases}/{out}/")
-         run(f"rm -fr {releases}/{out}/web_static")
-         run(f"rm -fr /data/web_static/current")
-         run(f"ln -s {releases}/{out}/ /data/web_static/current")
-    except Exception:
-         return False
-    print("New version deployed!")
     return True
